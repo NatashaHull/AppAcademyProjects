93ccdcd875d128734eaac1a44cb4d62d6c629ebe
Natasha Hull <musicalnat@gmail.com>
2013-09-21 11:27:55 -0700

    Created a working tic-tac-toe ai and fixed some bugs in the initial code for that problem

--- W1D5/tic_tac_toe_ai2.rb
+++ W1D5/tic_tac_toe_ai2.rb
@@ -1,8 +1,3 @@
-#Comments on what follows:
-#This code became so buggy from trying to implement methods at once, I was unable to get
-#very far with it. I would be happy with someone fixing this code, but I have done almost
-#all that I can with it for now.  If you want to see a working Tic-Tac-Toe AI, look at the
-#tic_tac_toe_ai2.rb file in this directory.
 require 'set'
 
 class TreeNode
@@ -56,81 +51,14 @@ class TreeNode
     end
   end
 
-  def minimax(target)
-    print "."
-    max_action(target, 3)
-  end
-
-  def max_action(target, depth)
-    #takes in target (:x or :o) and
-    #returns best possible action (e.g. [1,2])
-    return minimax_result(target) if @children.empty?
-    return self.evaluation_function(target) if depth == 0
-    min_value = -1000
-    best_child = @children.first
-    @children.each do |child|
-      child_result = child.min_action(target, depth-1)
-      if child_result > min_value
-        min_value = child_result
-        best_child = child
-      end
-    end
-    return best_child if depth == 3
-    min_value
-  end
-
-  def min_action(target, depth)
-    return minimax_result(target) if @children.empty? || @value.over?
-    max_value = 1000
-    @children.each do |child|
-      child_result = child.max_action(target, depth)
-      if child_result < max_value
-        max_value = child_result
-      end
-    end
-    max_value
-  end
-
-  def minimax_result(target)
-    #for a leaf node tells you if it is a winner or loser where target is :x or :o
-    #return 1 for winner, 0 for tie, -1 for loser
-
-    return 0 if @value.tied? #potentially memoize this result
-    if @value.winner == target
-      return 100
-    else
-      return -100
-    end
-  end
-
-  def evaluation_function(target)
-    target_two_in_a_row = 0
-    other_two_in_a_row = 0
-    target_corners_taken = 0
-    other_corners_taken = 0
-    (@value.rows + @value.cols + @value.diagonals).each do |triple|
-      target_corners_taken += 1 if triple[0] == target
-      other_corners_taken += 1 unless triple[0].nil?
-      if triple[0] == triple[1] || triple[0] == triple[2]
-        triple[0] == target ? target_two_in_a_row += 1 : other_two_in_a_row += 1
-      elsif triple[1] == triple[2]
-        triple[1] == target ? target_two_in_a_row += 1 : other_two_in_a_row += 1
-      end
-    end
-
-    if @value.rows[1][1] == target
-      center_value = 1
-    elsif @value.rows[1][1].nil?
-      center_value = 0
-    else
-      center_value = -1
-    end
-    (4 * center_value) + (2 * target_two_in_a_row) + target_corners_taken - (3 * other_two_in_a_row) - other_corners_taken
-  end
-
   def path_to_root
     return [@value] unless @parent
     return [@value] + @parent.path_to_root
+  end
+
+  def path_to_prior_node(nth_parent)
+    return self if self.parent == nth_parent
+    self.parent.path_to_prior_node(nth_parent)
   end
 end
 
@@ -344,11 +272,15 @@ class ComputerPlayer
 
   private
   def winner_move(game, mark)
-    current_node = game.game_tree.bfs { |other_board| game.board.same_board(other_board) }
-    best_child = current_node.minimax(mark)
-    return best_child.pos if game.board.empty?(best_child.pos)
-    # no winning move
-    nil
+    start_node = game.game_tree.bfs { |other_board| game.board.same_board(other_board) }
+    start_node.children.each do |child|
+      return child.path_to_prior_node(start_node).pos if child.value.won? #Returns a winning move if you can make one.
+      child.children.each do |grandchild|
+        return grandchild.pos if grandchild.value.won? #Returns a move that blocks the other player from winning
+      end
+    end
+
+    nil #Move randomly otherwise
   end
 
   def random_move(game, mark)
