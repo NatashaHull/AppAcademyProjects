906b8d7f3d4a077e66e7c7e4c00f6e3b6b96ad24
Natasha Hull <musicalnat@gmail.com>
2013-10-05 21:32:31 -0700

    first blogger commit

---
+++ Weekends/blogger/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blogger::Application.config.secret_token = '63bf7a2a7d52494bba8f0e744bf70713331057d528a0879a08076a757cf44a6d178f813cb7873e12f5ba80861c16ec849efc159fd0c6f3f555243afb43a6691e'
---
+++ Weekends/blogger/config/routes.rb
@@ -0,0 +1,3 @@
+Blogger::Application.routes.draw do
+  resources :articles
+end
