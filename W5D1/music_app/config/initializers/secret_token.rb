2b76555cedcc07b5d6c969c0e264af133d8360af
Natasha Hull <musicalnat@gmail.com>
2013-10-14 10:09:58 -0700

    Starting music app

---
+++ W5D1/music_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MusicApp::Application.config.secret_token = '00f02cb8f3a8a056a99f56d08910dbd33b48ffc46ae7df6e0e598e2196cab04decd621170731a9af78f032dadc7926c039ab11116b26165bbed2426e727c5fca'
