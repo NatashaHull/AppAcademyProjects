fddf7f6c01d813b576c9befc43bd28fb809e2fa0
Natasha Hull <musicalnat@gmail.com>
2013-10-14 17:42:37 -0700

    Finished adding musical tags to lyrics that also had pre tags

--- W5D1/music_app/app/helpers/tracks_helper.rb
+++ W5D1/music_app/app/helpers/tracks_helper.rb
@@ -1,9 +1,9 @@
 module TracksHelper
   def show_lyrics(lyrics)
-    m_lyrics = lyrics.gsub("\r\n", "\r\n♫ ")
+    m_lyrics = h(lyrics).gsub("\r\n", "\r\n&#9835; ")
     html = "<pre>"
     html += "&#9835; "
-    html += h(m_lyrics)
+    html += m_lyrics
     html += "</pre>"
     html.html_safe
   end
