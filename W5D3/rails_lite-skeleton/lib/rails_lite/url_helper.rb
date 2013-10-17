2874f84613f473d22f8602d20dbe3171e096cfa0
Natasha Hull <musicalnat@gmail.com>
2013-10-16 17:43:39 -0700

    Fixed a minor bug

--- W5D3/rails_lite-skeleton/lib/rails_lite/url_helper.rb
+++ W5D3/rails_lite-skeleton/lib/rails_lite/url_helper.rb
@@ -1,7 +1,8 @@
 module UrlHelper
-  def create_url_helper(root_url = "localhost:8080", path)
+  def create_url_helper(root_url="http://localhost:8080", path)
     #I am assuming there are not router params for the sake
     #of simplicity
+    helper_name = path.split('/').join('_')
     define_method("#{helper_name}_url") do
       url = root_url
       url += path
