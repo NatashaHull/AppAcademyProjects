cabcfc776c1b8895f8fc6cd71a8d1b7a94334f07
Natasha Hull <musicalnat@gmail.com>
2013-10-28 08:39:01 -0700

    Did practice assessment

--- Weekends/assessment-prep-master/a05-practice/src/js_assessment.js
+++ Weekends/assessment-prep-master/a05-practice/src/js_assessment.js
@@ -1,15 +1,87 @@
 Assessment = {};
 
-Assessment.mergeSort = function(array) {};
+Assessment.mergeSort = function(array) {
+  if(array.length <= 1) {
+    return array;
+  }
 
-Assessment.recursiveExponent = function(base, power) {};
+  var leftArr = array.slice(0, array.length/2);
+  var rightArr = array.slice(array.length/2);
+  var arr1 = Assessment.mergeSort(leftArr);
+  var arr2 = Assessment.mergeSort(rightArr);
+  return Assessment.merge(arr1, arr2);
+};
 
-Assessment.powCall = function (base, power, callback) {};
+Assessment.merge = function(arr1, arr2) {
+  var mergedArray = [];
+  if(arr1.length > 0 && arr2.length > 0) {
+    if(arr1[0] < arr2[0]) {
+      mergedArray.push(arr1.shift());
+    } else {
+      mergedArray.push(arr2.shift());
+    }
+  } else if(arr1.length === 0) {
+    return arr2;
+  } else {
+    return arr1;
+  }
 
-Assessment.transpose = function (matrix) {};
+  return mergedArray.concat(Assessment.merge(arr1, arr2));
+};
 
-Assessment.primes = function (n) {};
+Assessment.recursiveExponent = function(base, power) {
+  if(power === 0) {
+    return 1;
+  } else {
+    return base * Assessment.recursiveExponent(base, power-1);
+  }
+};
 
-Function.prototype.myApply = function (context, argArray) {};
+Assessment.powCall = function (base, power, callback) {
+  var result = Assessment.recursiveExponent(base, power);
+  callback(result);
+  return result
+};
 
-Function.prototype.inherits = function (Parent) {};
+Assessment.transpose = function (matrix) {
+  var transposed_arr = []
+  for(var i = 0; i < matrix.length; i++) {
+    transposed_row = []
+    for(var j=0; j < matrix[i].length; j++) {
+      transposed_row.push(matrix[j][i]);
+    }
+    transposed_arr.push(transposed_row);
+  }
+  return transposed_arr;
+};
+
+Assessment.primes = function (n) {
+  primesArr = [];
+  var prime = 2;
+  while(n > primesArr.length) {
+    if(Assessment.isPrime(prime)) {
+      primesArr.push(prime);
+    }
+    prime++;
+  }
+  return primesArr;
+};
+
+Assessment.isPrime = function(num) {
+  for(var i = 2; i <= Math.sqrt(num); i++) {
+    if(num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+Function.prototype.myCall = function (context, argArray) {
+  return this.bind(context, argArray)();
+};
+
+Function.prototype.inherits = function (Parent) {
+  function Substitute() {};
+  Substitute.prototype = Parent.prototype;
+  this.prototype = new Substitute;
+};
--- Weekends/assessment-prep-master/a05-practice/src/wakeboard_game.js
+++ Weekends/assessment-prep-master/a05-practice/src/wakeboard_game.js
@@ -1,1 +1,28 @@
 // WakeboardGame module
+(function() {
+  var WakeboardGame = window.WakeboardGame = (WakeboardGame || {});
+
+  var Boat = WakeboardGame.Boat = function(sponsor) {
+    this.sponsor = sponsor;
+  };
+
+  Boat.prototype.power = function() { return 'power'; };
+  Boat.prototype.turn= function() { return 'turn'; };
+  Boat.prototype.sink = function() { return 'sink'; };
+
+  var Wakeboarder = WakeboardGame.Wakeboarder = function(name, sponsor) {
+    this.name = name;
+    this.sponsor = sponsor;
+  };
+
+  Wakeboarder.prototype.jump = function() { return 'jump'; };
+  Wakeboarder.prototype.spin = function() { return 'spin'; };
+  Wakeboarder.prototype.grind = function() { return 'grind'; };
+  Wakeboarder.prototype.crash = function() { return 'crash'; };
+
+  expect(wakeboarder.sponsor).toEqual('Hyperlite')
+    expect(wakeboarder.jump()).toEqual('jump');
+    expect(wakeboarder.spin()).toEqual('spin');
+    expect(wakeboarder.grind()).toEqual('grind');    
+    expect(wakeboarder.crash()).toEqual('crash')
+})();
