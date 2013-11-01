d0b1df23508fc880977208d2ff49b0316341a8bd
Natasha Hull <musicalnat@gmail.com>
2013-11-01 11:08:30 -0700

    Initialized backbone

---
+++ Week7/W7D5/news-reader-demo-master/app/assets/javascripts/new_reader.js
@@ -0,0 +1,13 @@
+window.NewReader = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+    alert('Hello from Backbone!');
+  }
+};
+
+$(document).ready(function(){
+  NewReader.initialize();
+});
